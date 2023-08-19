010c9b50b1a85629d244b7b0d5e171c55d884a1e
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-19 15:12:25 +0500

    Rewrite Phonebook constructor

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ class Phonebook:
 
     def __init__(self) -> None:
         self.file = Path("phonebook.csv")
-        with open(self.file, "r", encoding="utf-8", newline="") as f:
-            reader = DictReader(f)
-            self.headers: list[str] = list(reader.fieldnames)
-            self.records: list[dict] = list(reader)
+        self.fieldnames = ("first_name", "middle_name", "last_name", "organization", "phone_work", "phone_personal")
+
+        if not self.file.exists():
+            with open(self.file, "w", encoding="utf-8", newline="") as f:
+                writer = DictWriter(f, fieldnames=self.fieldnames)
+                writer.writeheader()
+        else:
+            with open(self.file, "r", encoding="utf-8", newline="") as f:
+                reader = DictReader(f)
+                self.records: list[dict] = list(reader)
 
     def display(self) -> None:
         """TODO: rewrite using some module for pretty tables"""
-        print(*(f"{header:<16}" for header in self.headers), sep="|")
+        print(*(f"{field:<16}" for field in self.fieldnames), sep="|")
         for record in self.records:
             print(*(f"{value:<16}" for value in record.values()), sep="|")
 
     def add(self, record: list[str]) -> None:
         with open(self.file, "a", encoding="utf-8", newline="") as f:
-            writer = DictWriter(f, fieldnames=self.headers)
-            writer.writerow({k: v for k, v in zip(self.headers, record)})
+            writer = DictWriter(f, fieldnames=self.fieldnames)
+            writer.writerow({k: v for k, v in zip(self.fieldnames, record)})
 
     def edit(self) -> None:
         pass
@@ -35,9 +41,7 @@ class Phonebook:
 
 def main():
     phonebook = Phonebook()
-    phonebook.display()
-    phonebook.add(["f", "m", "l", "org", "p1", "p2"])
-    phonebook.display()
+    phonebook.add(["Иван", "Иванович", "Иванов", "Яндекс", "+79220000000", "+79221111111"])
 
 
 if __name__ == "__main__":
