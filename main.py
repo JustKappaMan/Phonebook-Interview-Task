e02d26b57eb9767780c8babd11af20f95e987372
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-19 14:15:24 +0500

    Basic initialization and output

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from csv import DictReader
 from dataclasses import dataclass
 
 
@@ -16,11 +18,18 @@ class Record:
 class Phonebook:
     """The main class that contains and manipulates all the records"""
 
-    def __init__(self):
-        self.records: list[Record] = ...
+    def __init__(self) -> None:
+        self.file = Path("phonebook.csv")
+        with open(self.file, "r", encoding="utf-8", newline="") as f:
+            reader = DictReader(f)
+            self.headers = reader.fieldnames
+            self.records = list(reader)
 
     def display(self):
-        pass
+        """TODO: rewrite using some module for pretty tables"""
+        print(*(f"{header:<16}" for header in self.headers), sep="|")
+        for record in self.records:
+            print(*(f"{value:<16}" for value in record.values()), sep="|")
 
     def add(self):
         pass
@@ -36,7 +45,8 @@ class Phonebook:
 
 
 def main():
-    pass
+    phonebook = Phonebook()
+    phonebook.display()
 
 
 if __name__ == "__main__":
