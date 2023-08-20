ba9f8d05e8a892b3aec4c1d55a3e17ebd94d1733
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-20 14:39:29 +0500

    Add comments and refactor misc.py

--- src/misc.py
+++ src/misc.py
@@ -1,13 +1,14 @@
 from os import system
 from sys import platform
-from typing import Iterable
 from itertools import islice
 
 
 def clear_screen() -> None:
+    """Clear command line on any platform"""
     system("cls" if platform == "win32" else "clear")
 
 
-def chunk(it: Iterable, size: int = 10) -> list[tuple]:
+def chunk(it: list, size: int = 10) -> list[tuple]:
+    """Split a list into equally-sized (except the last one) tuples"""
     it = iter(it)
     return list(iter(lambda: tuple(islice(it, size)), ()))
