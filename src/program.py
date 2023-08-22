7baa916ddcc466fb3ebcb38bd3c58f9ed320d9e1
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-08-22 14:48:12 +0500

    `guarded_import()` is method now
    Also, make `__clear_screen()` and `__guarded_input()` "private"

--- src/program.py
+++ src/program.py
@@ -11,18 +11,6 @@ def chunk(it: list, size: int = 10) -> list[tuple]:
     return list(iter(lambda: tuple(itertools.islice(it, size)), ()))
 
 
-def guarded_input(prompt: str, length: int = 16, clear_screen: bool = False) -> str:
-    """To accept data that will fit in table column"""
-    while True:
-        if clear_screen:
-            Program.clear_screen()
-
-        input_data = input(prompt)
-
-        if len(input_data) <= length:
-            return input_data
-
-
 class Program:
     """Basically, the UI class that uses Phonebook to manipulate all the records"""
 
@@ -37,7 +25,7 @@ class Program:
     def __render_main_menu(self) -> None:
         """Main menu"""
         while True:
-            Program.clear_screen()
+            Program.__clear_screen()
             user_input = input(
                 "Телефонный справочник\n\n"
                 "1. Просмотреть записи\n"
@@ -68,7 +56,7 @@ class Program:
         current_page_index = 0
 
         while True:
-            Program.clear_screen()
+            Program.__clear_screen()
             print(f"Телефонный справочник (c. {current_page_index + 1})\n")
             self.__print_table(pages[current_page_index])
 
@@ -97,12 +85,14 @@ class Program:
         # It's generated automatically, not received from user input
         new_record = {"ID": len(self.phonebook.records) + 1}
         for name in self.phonebook.fieldnames[1:]:
-            new_record[name] = guarded_input(f"Телефонный справочник (новая запись)\n\n{name}: ", clear_screen=True)
+            new_record[name] = self.__guarded_input(
+                f"Телефонный справочник (новая запись)\n\n{name}: ", clear_screen=True
+            )
 
         self.phonebook.add(new_record)
 
         while True:
-            Program.clear_screen()
+            Program.__clear_screen()
             user_input = input(
                 "Телефонный справочник (новая запись)\n\n"
                 "Запись успешно добавлена!\n\n"
@@ -122,7 +112,7 @@ class Program:
     def __render_editing_section(self) -> None:
         """Menu section to edit an existing record in DB"""
         while True:
-            user_input = guarded_input(
+            user_input = self.__guarded_input(
                 "Телефонный справочник (редактирование записи)\n\nВведите ID записи, подлежащей редактированию: ",
                 clear_screen=True,
             )
@@ -133,7 +123,7 @@ class Program:
                     break
 
         while True:
-            Program.clear_screen()
+            Program.__clear_screen()
             print("Телефонный справочник (редактирование записи)\n")
             self.__print_table([self.phonebook.records[record_id - 1]])
 
@@ -142,14 +132,14 @@ class Program:
                 case "1":
                     new_record = {"ID": record_id}
                     for name in self.phonebook.fieldnames[1:]:
-                        new_record[name] = guarded_input(
+                        new_record[name] = self.__guarded_input(
                             f"Телефонный справочник (редактирование записи)\n\n{name}: ", clear_screen=True
                         )
 
                     self.phonebook.edit(record_id, new_record)
 
                     while True:
-                        Program.clear_screen()
+                        Program.__clear_screen()
                         user_input = input(
                             "Телефонный справочник (редактирование записи)\n\n"
                             "Запись успешно отредактирована!\n\n"
@@ -175,7 +165,7 @@ class Program:
         search_criteria = dict.fromkeys(self.phonebook.fieldnames)
 
         while True:
-            Program.clear_screen()
+            Program.__clear_screen()
             user_input = input(
                 "Телефонный справочник (поиск по записям)\n\n"
                 "Пожалуйста, задайте критерии для поиска\n\n"
@@ -191,41 +181,41 @@ class Program:
                 "Введите номер пункта меню: "
             )
 
-            Program.clear_screen()
+            Program.__clear_screen()
             match user_input:
                 case "1":
-                    search_criteria["ID"] = guarded_input(
+                    search_criteria["ID"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите ID: ", clear_screen=True
                     )
                 case "2":
-                    search_criteria["Имя"] = guarded_input(
+                    search_criteria["Имя"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите имя: ", clear_screen=True
                     )
                 case "3":
-                    search_criteria["Отчество"] = guarded_input(
+                    search_criteria["Отчество"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите отчество: ", clear_screen=True
                     )
                 case "4":
-                    search_criteria["Фамилия"] = guarded_input(
+                    search_criteria["Фамилия"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите фамилию: ", clear_screen=True
                     )
                 case "5":
-                    search_criteria["Организация"] = guarded_input(
+                    search_criteria["Организация"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите организацию: ", clear_screen=True
                     )
                 case "6":
-                    search_criteria["Рабочий телефон"] = guarded_input(
+                    search_criteria["Рабочий телефон"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите рабочий телефон: ", clear_screen=True
                     )
                 case "7":
-                    search_criteria["Личный телефон"] = guarded_input(
+                    search_criteria["Личный телефон"] = self.__guarded_input(
                         "Телефонный справочник (поиск по записям)\n\nВведите личный телефон: ", clear_screen=True
                     )
                 case "8":
                     if not (found_records := self.phonebook.search(search_criteria, is_strict=self.search_is_strict)):
                         # "Nothing was found" menu section
                         while True:
-                            Program.clear_screen()
+                            Program.__clear_screen()
                             user_input = input(
                                 "Телефонный справочник (результаты поиска)\n\n"
                                 "Поиск с заданными критериями не дал результата\n\n"
@@ -247,7 +237,7 @@ class Program:
                         current_page_index = 0
 
                         while True:
-                            Program.clear_screen()
+                            Program.__clear_screen()
                             print(f"Телефонный справочник (результаты поиска c. {current_page_index + 1})\n")
                             self.__print_table(pages[current_page_index])
 
@@ -282,12 +272,23 @@ class Program:
         for record in records:
             print(*(value.ljust(self.max_field_length) for value in record.values()), sep="|")
 
+    def __guarded_input(self, prompt: str, clear_screen: bool = False) -> str:
+        """To accept data that will fit in table column"""
+        while True:
+            if clear_screen:
+                Program.__clear_screen()
+
+            input_data = input(prompt)
+
+            if len(input_data) <= self.max_field_length:
+                return input_data
+
     @staticmethod
-    def clear_screen() -> None:
+    def __clear_screen() -> None:
         """Clear command line on any platform"""
         os.system("cls" if sys.platform == "win32" else "clear")
 
     @staticmethod
     def close() -> None:
-        Program.clear_screen()
+        Program.__clear_screen()
         sys.exit()
